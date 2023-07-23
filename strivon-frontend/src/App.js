a5f11a727487e8dafcbd9b3173cf1c987c740e1c
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-23 23:08:14 +0100

    Added routes of the pages

--- strivon-frontend/src/App.js
+++ strivon-frontend/src/App.js
@@ -1,10 +1,15 @@
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
+import DashboardPage from "./pages/HomePage/Dashboard/Dashboard.page";
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/student" element={<DashboardPage />} />
+      </Routes>
     </div>
   );
 }
