409f4883be4f41d094a0034b232585d3c1abe611
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-27 11:06:14 +0100

    Implemented reduc to get the state of the toggle button and added the Link and onClick functionality

--- strivon-frontend/src/dashboard-scenes/MenuItems/DashMenu.jsx
+++ strivon-frontend/src/dashboard-scenes/MenuItems/DashMenu.jsx
@@ -5,29 +5,59 @@ import { TbStack2 } from "react-icons/tb";
 import { HiOutlineAcademicCap } from "react-icons/hi";
 import { MdPercent } from "react-icons/md";
 
-const DashMenu = () => {
+import { Link } from "react-router-dom";
+import { setMobileView } from "../../redux/mobileToggle/mobileToggleAction";
+import { createStructuredSelector } from "reselect";
+import { selectMobileView } from "../../redux/mobileToggle/mobileToggleSelect";
+import { connect } from "react-redux";
+
+const DashMenu = ({ setToggle }) => {
   return (
     <section className={styles.dashmenu}>
-      <div className={styles.item}>
-        <AiOutlineUser className={styles.icon}/>
-        Profile
-      </div>
-      <div className={styles.item} >
-        <TbStack2 className={styles.icon}/>
-        Payments
-      </div>
-      <div className={styles.item}>
-        <HiOutlineAcademicCap className={styles.icon}/>
-        Courses
-      </div>
-      <div className={styles.item}>
-        <MdPercent className={styles.icon}/> Result
-      </div>
-      <div className={styles.item}>
-        <img src="./assets/icons/referral.svg" alt="referral" className={styles.icon}/> Referral
-      </div>
+      <Link to="profile" onClick={() => setToggle(false)}>
+        <div className={styles.item}>
+          <AiOutlineUser className={styles.icon} />
+          Profile
+        </div>
+      </Link>
+      <Link to="payments" onClick={() => setToggle(false)}>
+        <div className={styles.item}>
+          <TbStack2 className={styles.icon} />
+          Payments
+        </div>
+      </Link>
+
+      <Link to="courses" onClick={() => setToggle(false)}>
+        <div className={styles.item}>
+          <HiOutlineAcademicCap className={styles.icon} />
+          Courses
+        </div>
+      </Link>
+      <Link to="results" onClick={() => setToggle(false)}>
+        <div className={styles.item}>
+          <MdPercent className={styles.icon} /> Result
+        </div>
+      </Link>
+      <Link to="referral" onClick={() => setToggle(false)}>
+        <div className={styles.item}>
+          <img
+            src="../assets/icons/referral.svg"
+            alt="referral"
+            className={styles.icon}
+          />{" "}
+          Referral
+        </div>
+      </Link>
     </section>
   );
 };
 
-export default DashMenu;
+const mapStateToProps = createStructuredSelector({
+  toggle: selectMobileView,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  setToggle: () => dispatch(setMobileView()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(DashMenu);
