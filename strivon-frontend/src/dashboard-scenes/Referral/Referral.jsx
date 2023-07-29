06c7f7ea4172e711e89022122af82fcf8cf548ea
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-29 01:43:00 +0100

    updated page

--- strivon-frontend/src/dashboard-scenes/Referral/Referral.jsx
+++ strivon-frontend/src/dashboard-scenes/Referral/Referral.jsx
@@ -3,7 +3,9 @@ import styles from './referral.module.css';
 
 const Referral = () => {
   return (
-    <div className={styles.referral}>Referral</div>
+    <div className={styles.referral}>
+      <h2>Coming soon...</h2>
+    </div>
   )
 }
 
