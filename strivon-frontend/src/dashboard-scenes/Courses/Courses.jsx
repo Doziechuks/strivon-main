e39fa6a0daa505f4a5b3f67434ef695b8bdd8909
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-29 01:41:40 +0100

    Updated the course page

--- strivon-frontend/src/dashboard-scenes/Courses/Courses.jsx
+++ strivon-frontend/src/dashboard-scenes/Courses/Courses.jsx
@@ -1,13 +1,50 @@
-import styles from './courses.module.css';
+import styles from "./courses.module.css";
 
-
-
-import React from 'react'
+import React from "react";
 
 const Courses = () => {
   return (
-    <div className={styles.courses}>Courses</div>
-  )
-}
+    <div className={styles.courses}>
+      <div className={styles.wrap}>
+        <h2>Our Packages</h2>
+        <div className={styles.packagewrap}>
+          <article>
+            <p className={styles.desc}>Frontend Software Engineering, Level 1</p>
+            <ul className={styles.listcontainer}>
+              <li>HTML5</li>
+              <li>CSS</li>
+              <li>Javascript (DOM, BOM & Algorithms)</li>
+              <li>Git and Github</li>
+            </ul>
+          </article>
+          <article>
+            <p className={styles.desc}>Frontend Software Engineering, Level 2</p>
+            <ul className={styles.listcontainer}>
+              <li>React</li>
+              <li>Material UI</li>
+              <li>Javascript (Algorithms)</li>
+              <li>Tailwind css</li>
+              <li>APIs and endpoints testing</li>
+            </ul>
+          </article>
+          <article>
+            <p className={styles.desc}>Backend Software Engineering</p>
+            <ul className={styles.listcontainer}>
+              <li>PHP</li>
+              <li>Nodejs</li>
+            </ul>
+          </article>
+          <article>
+            <p className={styles.desc}>Mobile App development</p>
+            <ul className={styles.listcontainer}>
+              <li>React native</li>
+              <li>Flutter</li>
+            </ul>
+          </article>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Courses
+export default Courses;
