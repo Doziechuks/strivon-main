c4110f0ec4df64179ba6c0a786b942fe0eb59be1
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-29 01:12:02 +0100

    Added the route to the faq page

--- strivon-frontend/src/App.js
+++ strivon-frontend/src/App.js
@@ -7,6 +7,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/HomePage/HomePage";
 import ContactPage from "./pages/contactPage/ContactPage";
 import Register from "./pages/registrationPage/Register";
+import Faq from "./pages/contactPage/FaqPage/faq.page";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/contactus" element={<ContactPage />} />
         <Route exact path="/apply" element={<Register />} />
+        <Route exact path="/faqs" element={<Faq />} />
         <Route path="/student/*" element={<DashboardPage />} />
       </Routes>
     </div>
