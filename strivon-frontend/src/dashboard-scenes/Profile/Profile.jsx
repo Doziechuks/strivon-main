7c250b15abd934d67ff474378a1e9de4a88e4ef8
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-30 15:02:48 +0100

    added an 'add photo' in th p element without functionality

--- strivon-frontend/src/dashboard-scenes/Profile/Profile.jsx
+++ strivon-frontend/src/dashboard-scenes/Profile/Profile.jsx
@@ -1,5 +1,6 @@
-import styles from './profile.module.css';
-import { userdata } from '../../userdata';
+import styles from "./profile.module.css";
+import { userdata } from "../../userdata";
+import { MdAddAPhoto } from "react-icons/md";
 
 const Profile = () => {
   const { firstName, lastName, email, gender } = userdata;
@@ -11,52 +12,52 @@ const Profile = () => {
     <div className={styles.profile}>
       <section className={styles.wrapper}>
         <div className={styles.heading}>
-        <h2>Personal Information</h2>
-        <article>
-          <p>Student ID:</p>
-          <p>Course plan:</p>
-        </article>
+          <h2>Personal Information</h2>
+          <article>
+            <p>Student ID:</p>
+            <p>Course plan:</p>
+          </article>
         </div>
 
-        <div className={styles.usericon}>
-          <object type="image/jpeg" data="">
-            initials
-          </object>
-          <p>{initials}</p>
+        <div className={styles.profileimagewrap}>
+          <div className={styles.usericon}>
+            <object type="image/jpeg" data="">
+              initials
+            </object>
+            <p>{initials}</p>
+          </div>
+          <p className={styles.add}><MdAddAPhoto /> Add photo</p>
         </div>
         <div className={styles.formwrap}>
           <form>
             <div className={styles.formgroup}>
-                <label htmlFor="">First Name</label>
-                <input type="text" value={firstName}/>
+              <label htmlFor="">First Name</label>
+              <input type="text" value={firstName} />
             </div>
             <div className={styles.formgroup}>
-                <label htmlFor="">Last Name</label>
-                <input type="text" value={firstName}/>
+              <label htmlFor="">Last Name</label>
+              <input type="text" value={firstName} />
             </div>
             <div className={styles.formgroup}>
-                <label htmlFor="">Email Address</label>
-                <input type="text" value={email}/>
+              <label htmlFor="">Email Address</label>
+              <input type="text" value={email} />
             </div>
             <div className={styles.formgroup}>
-                <label htmlFor="">Gender</label>
-                <select name="gender" id="">
-                  <option value={gender}>{gender}</option>
-                  <option value='female'>female</option>
-                </select>
+              <label htmlFor="">Gender</label>
+              <select name="gender" id="">
+                <option value={gender}>{gender}</option>
+                <option value="female">female</option>
+              </select>
             </div>
             <div className={styles.formgroup}>
-                <label htmlFor="">First Name</label>
-                <input type="text" value={firstName}/>
+              <label htmlFor="">First Name</label>
+              <input type="text" value={firstName} />
             </div>
           </form>
         </div>
-      
-
       </section>
-      
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
