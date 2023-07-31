79db88cf94c2ac0e147d23b5d88c783f0e00ab2f
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-07-31 21:29:50 +0100

    Added useLocation in order to add css effect on active paths

--- strivon-frontend/src/dashboard-scenes/MenuItems/DashMenu.jsx
+++ strivon-frontend/src/dashboard-scenes/MenuItems/DashMenu.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./dashmenu.module.css";
-import { AiOutlineUser } from "react-icons/ai";
+import { AiFillTaobaoSquare, AiOutlineUser } from "react-icons/ai";
 import { TbStack2 } from "react-icons/tb";
 import { HiOutlineAcademicCap } from "react-icons/hi";
 import { MdPercent } from "react-icons/md";
@@ -10,36 +10,45 @@ import { setMobileView } from "../../redux/mobileToggle/mobileToggleAction";
 import { createStructuredSelector } from "reselect";
 import { selectMobileView } from "../../redux/mobileToggle/mobileToggleSelect";
 import { connect } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 const DashMenu = ({ setToggle }) => {
+  const [pathName, setPathName] = useState("");
+  const location = useLocation();
+  const { pathname } = location;
+    useEffect(() => {
+    setPathName(pathname);
+  }, [pathname]);
+
   return (
     <section className={styles.dashmenu}>
       <Link to="profile" onClick={() => setToggle(false)}>
-        <div className={styles.item}>
+        <div className={`${styles.item} ${pathName.includes('profile') ? styles.active : ''}`}>
           <AiOutlineUser className={styles.icon} />
           Profile
         </div>
       </Link>
       <Link to="payments" onClick={() => setToggle(false)}>
-        <div className={styles.item}>
+        <div className={`${styles.item} ${pathName.includes('payments') ? styles.active : ''}`}>
           <TbStack2 className={styles.icon} />
           Payments
         </div>
       </Link>
 
       <Link to="courses" onClick={() => setToggle(false)}>
-        <div className={styles.item}>
+        <div className={`${styles.item} ${pathName.includes('courses') ? styles.active : ''}`}>
           <HiOutlineAcademicCap className={styles.icon} />
           Courses
+    
         </div>
       </Link>
       <Link to="results" onClick={() => setToggle(false)}>
-        <div className={styles.item}>
+        <div className={`${styles.item} ${pathName.includes('results') ? styles.active : ''}`}>
           <MdPercent className={styles.icon} /> Result
         </div>
       </Link>
       <Link to="referral" onClick={() => setToggle(false)}>
-        <div className={styles.item}>
+        <div className={`${styles.item} ${pathName.includes('referral') ? styles.active : ''}`}>
           <img
             src={referralicon}
             alt="referral"
