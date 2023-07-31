ba32da1c2d2a3ce598daa5446ee98904b0c4b9db
doziechuks <doziechuks1010@gmail.com>
2023-07-31 21:33:44 +0100

    updated the appjs and the dashboard

--- strivon-frontend/src/App.js
+++ strivon-frontend/src/App.js
@@ -9,10 +9,13 @@ import HomePage from "./pages/HomePage/HomePage";
 import ContactPage from "./pages/contactPage/ContactPage";
 import Register from "./pages/registrationPage/Register";
 import Faq from "./pages/FaqPage/faq.page";
+import Frontend from "./pages/tracksPages/frontendTrack/Frontend";
+import Backend from "./pages/tracksPages/backendTrack/Backend";
 
 // protected routes
 import Login from "./pages/loginPage/Login";
 import DashboardPage from "./pages/Dashboard/Dashboard.page";
+import MobileApp from "./pages/tracksPages/mobileAppTrack/MobileApp";
 
 function App() {
   return (
@@ -24,6 +27,11 @@ function App() {
         <Route exact path="/contactus" element={<ContactPage />} />
         <Route exact path="/apply" element={<Register />} />
         <Route exact path="/faqs" element={<Faq />} />
+        <Route path="/tracks">
+          <Route path="frontend" element={<Frontend />} />
+          <Route path="backend" element={<Backend />} />
+          <Route path="mobileapp" element={<MobileApp />} />
+        </Route>
 
         {/* protected pages */}
         <Route path="/student/*" element={<DashboardPage />} />
--- strivon-frontend/src/pages/Dashboard/Dashboard.page.jsx
+++ strivon-frontend/src/pages/Dashboard/Dashboard.page.jsx
@@ -1,49 +1,47 @@
-import React from 'react';
-import styles from './Dashboard.module.css';
-import Sidebar from '../../dashboard-scenes/Sidebar/Sidebar.component';
-import Topbar from '../../dashboard-scenes/TopBar/Topbar';
-import MobileSidebar from '../../dashboard-scenes/MobileSidebar/MobileSidebar';
+import React from "react";
+import styles from "./Dashboard.module.css";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Sidebar from "../../dashboard-scenes/Sidebar/Sidebar.component";
+import Topbar from "../../dashboard-scenes/TopBar/Topbar";
+import MobileSidebar from "../../dashboard-scenes/MobileSidebar/MobileSidebar";
 
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { setMobileView } from '../../redux/mobileToggle/mobileToggleAction';
-import { selectMobileView } from '../../redux/mobileToggle/mobileToggleSelect';
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+import { setMobileView } from "../../redux/mobileToggle/mobileToggleAction";
+import { selectMobileView } from "../../redux/mobileToggle/mobileToggleSelect";
 
-import { Routes, Route } from 'react-router-dom';
-import Profile from '../../dashboard-scenes/Profile/Profile';
-import Courses from '../../dashboard-scenes/Courses/Courses';
-import Payments from '../../dashboard-scenes/Payments/Payments';
-import Results from '../../dashboard-scenes/Results/Results';
-import Referral from '../../dashboard-scenes/Referral/Referral';
+import Profile from "../../dashboard-scenes/Profile/Profile";
+import Courses from "../../dashboard-scenes/Courses/Courses";
+import Payments from "../../dashboard-scenes/Payments/Payments";
+import Results from "../../dashboard-scenes/Results/Results";
+import Referral from "../../dashboard-scenes/Referral/Referral";
 
-const DashboardPage = ({toggle, setToggle}) => {
+const DashboardPage = ({ toggle, setToggle }) => {
   return (
     <div className={styles.container}>
-        <Topbar />
-        <div className={styles.wrap}>
+      <Topbar />
+      <div className={styles.wrap}>
         <Sidebar />
         <MobileSidebar />
         <Routes>
-          <Route path='/profile' element={<Profile />}/>
-          <Route path='/courses' element={<Courses />}/>
-          <Route path='/payments' element={<Payments />}/>
-          <Route path='/results' element={<Results />}/>
-          <Route path='/referral' element={<Referral />}/>
+          <Route index element={<Navigate to="profile" />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/courses" element={<Courses />} />
+          <Route path="/payments" element={<Payments />} />
+          <Route path="/results" element={<Results />} />
+          <Route path="/referral" element={<Referral />} />
         </Routes>
-        </div>
-      
+      </div>
     </div>
-  )
-}
+  );
+};
 
 const mapStateToProps = createStructuredSelector({
   toggle: selectMobileView,
 });
 
-
 const mapDispatchToProps = (dispatch) => ({
   setToggle: () => dispatch(setMobileView()),
 });
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(DashboardPage);
