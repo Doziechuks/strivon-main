56e71f281008d0178df3d52b21cbb7c721a968cd
doziechuks <doziechuks1010@gmail.com>
2023-08-24 11:07:23 +0100

    updated the track pages

--- strivon-frontend/src/pages/tracksPages/mobileAppTrack/MobileApp.jsx
+++ strivon-frontend/src/pages/tracksPages/mobileAppTrack/MobileApp.jsx
@@ -1,7 +1,34 @@
 import React from "react";
+import styles from "../customstyle.module.css";
+import { trackData } from "../aboutTrackData";
+import CustomTrack from "../../../utils/customTrack/CustomTrack";
+import TaskAltIcon from "@mui/icons-material/TaskAlt";
 
 const MobileApp = () => {
-  return <div>mobile app</div>;
+  const { mobileApp } = trackData;
+  return (
+    <>
+      <CustomTrack
+        title="Become a certified Mobile App Developer"
+        subtitle="who is a mobile app developer"
+        about={mobileApp.about}
+        link1="/tracks/frontend"
+        track1="see frontend track"
+        link2="/tracks/backend"
+        track2="see backend track"
+      >
+        {mobileApp.courses.map((course, id) => (
+          <div className={styles.learnCard} key={id}>
+            <div className={styles.item} key={id}>
+              <TaskAltIcon />
+              <p>{course}</p>
+            </div>
+            <div className={styles.line} />
+          </div>
+        ))}
+      </CustomTrack>
+    </>
+  );
 };
 
 export default MobileApp;
