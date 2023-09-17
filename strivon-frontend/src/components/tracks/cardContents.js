975640a0195b794e82f44b5b87932450fc9fd79b
Ekwebenem Chukwuebuka Collins <collinschudy@gmail.com>
2023-09-17 13:29:43 +0100

    imported new images

--- strivon-frontend/src/components/tracks/cardContents.js
+++ strivon-frontend/src/components/tracks/cardContents.js
@@ -1,23 +1,27 @@
 import backend from '../../assets/images/backend track.jpg'
-import frontend from '../../assets/images/frontend track.jpg'
+import frontend from '../../assets/images/frontend track.jpg';
+import frontend1 from '../../assets/images/frontend track.jpg';
+import backend2 from '../../assets/images/backendd.jpg'
+import frontend2 from '../../assets/images/frontenddd.jpeg'
+import mobile2 from '../../assets/images/mobileappdev.jpg'
 import mobile from '../../assets/images/mobile app track.jpg'
 
 export const cardContents = [
     {
         id: 1,
-        imgUrl: frontend,
+        imgUrl: frontend2,
         title: "Frontend Web development",
         desc: "Lorem ipsum dolor pendulus and branching of emphasis prior to understudies",
     },
     {
         id: 2,
-        imgUrl: backend,
+        imgUrl: backend2,
         title: "Backend Web development",
         desc: "Lorem ipsum dolor rendering and patent colorful studies of web structure",
     },
     {
         id: 3,
-        imgUrl: mobile,
+        imgUrl: mobile2,
         title: "Mobile App development",
         desc: "Lorem ipsum dolor rendering and patent colorful studies of web structure",
     }
